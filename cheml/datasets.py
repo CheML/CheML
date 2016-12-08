070c101504df51a55faf19b747235a4a3e65c2bf
Kuang-Yu Samuel Chang <SamKChang@users.noreply.github.com>
2016-12-08 16:22:38 +0100

    ENH + REFACTOR Qm9 downloader (#18)
    * add qm9_loader
    
    * cleanup code
    
    * change funciton name _open_HF_pickle to _open_pickle
    
    * download compressed qm9 dataset instead

--- cheml/datasets.py
+++ cheml/datasets.py
@@ -11,6 +11,7 @@ import pickle
 from sklearn.datasets.base import Bunch
 from scipy.io import loadmat
 from sklearn.decomposition import PCA
+import tarfile
 
 
 def get_data_dirs(data_dir=None):
@@ -63,7 +64,8 @@ dataset_info = dict(
     HX4=("HF/HX4.pkl", HF_URL_BASE + "data_HX4.pkl"),
     HX5=("HF/HX5.pkl", HF_URL_BASE + "data_HX5.pkl"),
     HX6=("HF/HX6.pkl", HF_URL_BASE + "data_HX6.pkl"),
-    QM7=("GDB13/qm7.mat", "http://quantum-machine.org/data/qm7.mat")
+    QM7=("GDB13/qm7.mat", "http://quantum-machine.org/data/qm7.mat"),
+    QM9=("GDB13/qm9.pkl", "https://ndownloader.figshare.com/files/7003292")
     )
 
 
@@ -118,7 +120,24 @@ def _download(url, filename):
         raise
 
 
-def _get_or_download_dataset(dataset_name, path=None):
+def _tar_decompress(local_name, filename):
+
+    tar = tarfile.open(local_name)
+
+    # Assume single file member in tar file
+    members = tar.getnames()
+
+    if len(members) == 1:
+        content = tar.extractfile(members[0])
+        with open(filename, 'wb') as local_file:
+            local_file.write(content.read())
+    else:
+        tar.extractall(filename)
+
+    os.remove(local_name)
+
+
+def _get_or_download_dataset(dataset_name, path=None, suffix=None):
     rel_path, url = dataset_info[dataset_name]
     
     if path is None:
@@ -130,13 +149,23 @@ def _get_or_download_dataset(dataset_name, path=None):
         return filename
     else:
         filename = _get_first_writeable_path(paths, rel_path)
-        print("Downloading {} to {}...".format(url, filename))
-        _download(url, filename)
+        if suffix is not None:
+            local_name = filename + suffix
+        else:
+            local_name = filename
+        print("Downloading {} to {}...".format(url, local_name))
+        _download(url, local_name)
         print("... done.")
+
+        if suffix is not None:
+            if 'tar.gz' in suffix:
+                print("Decompress and tar file {}...".format(local_name))
+                _tar_decompress(local_name, filename)
+                print("... done.")
         return filename
 
 
-def _open_HF_pickle(filename):
+def _open_pickle(filename):
     # hack from http://stackoverflow.com/questions/11305790/pickle-incompatability-of-numpy-arrays-between-python-2-and-3
     # Needs to be extensively tested between versions
 
@@ -154,53 +183,72 @@ def _open_HF_pickle(filename):
 def load_HF2(path=None, large=False):
     dataset_name = 'HF2_7K' if large else 'HF2_1K'
     filename = _get_or_download_dataset(dataset_name, path=path)
-    return _open_HF_pickle(filename)
+    return _open_pickle(filename)
 
 def load_HF3(path=None, large=False):
     dataset_name = 'HF3_10K' if large else 'HF3_1K'
     filename = _get_or_download_dataset(dataset_name, path=path)
-    return _open_HF_pickle(filename)
+    return _open_pickle(filename)
 
 def load_HF4(path=None, large=False):
     dataset_name = 'HF4_10K' if large else 'HF4_1K'
     filename = _get_or_download_dataset(dataset_name, path=path)
-    return _open_HF_pickle(filename)
+    return _open_pickle(filename)
 
 def load_HF5(path=None, large=False):
     dataset_name = 'HF5_10K' if large else 'HF5_1K'
     filename = _get_or_download_dataset(dataset_name, path=path)
-    return _open_HF_pickle(filename)
+    return _open_pickle(filename)
 
 def load_HF6(path=None, large=False):
     dataset_name = 'HF6_10K' if large else 'HF6_1K'
     filename = _get_or_download_dataset(dataset_name, path=path)
-    return _open_HF_pickle(filename)
+    return _open_pickle(filename)
 
 
 def load_HX2(path=None):
     filename = _get_or_download_dataset('HX2', path=path)
-    return _open_HF_pickle(filename)
+    return _open_pickle(filename)
 
 def load_HX3(path=None):
     dataset_name = 'HX3'
     filename = _get_or_download_dataset(dataset_name, path=path)
-    return _open_HF_pickle(filename)
+    return _open_pickle(filename)
 
 def load_HX4(path=None):
     dataset_name = 'HX4'
     filename = _get_or_download_dataset(dataset_name, path=path)
-    return _open_HF_pickle(filename)
+    return _open_pickle(filename)
 
 def load_HX5(path=None):
     dataset_name = 'HX5'
     filename = _get_or_download_dataset(dataset_name, path=path)
-    return _open_HF_pickle(filename)
+    return _open_pickle(filename)
 
 def load_HX6(path=None):
     dataset_name = 'HX6'
     filename = _get_or_download_dataset(dataset_name, path=path)
-    return _open_HF_pickle(filename)
+    return _open_pickle(filename)
 
+def _gdb_align(bunch, align, only_planar, planarity_tol):
+    pca = PCA()
+    keep_molecule = []
+    for positions, charges in zip(bunch.R, bunch.Z):
+        transformed = np.vstack([
+            pca.fit_transform(positions[charges != 0]),
+            np.zeros([(charges == 0).sum(), 3])])
+        # the following evaluates how much variance is in the first two axes
+        # before this, the algorithm was also using zero positions, leading
+        # to 454 planar molecules (for QM7):
+        # pca.fit(positions).explained_variance_ratio_[:2].sum() #  
+        # currently, the algorithm yields 415 planar molecules
+        var_2D = pca.explained_variance_ratio_[:2].sum()
+        keep = (not only_planar) or var_2D > 1 - planarity_tol
+        keep_molecule.append(keep)
+        if align and keep:
+            positions[:] = transformed
+
+    return keep_molecule
 
 def load_qm7(path=None, align=False, only_planar=False, planarity_tol=.01):
     filename = _get_or_download_dataset("QM7", path=path)
@@ -209,22 +257,7 @@ def load_qm7(path=None, align=False, only_planar=False, planarity_tol=.01):
         if k in ['P', 'X', 'T', 'Z', 'R']})
     
     if align or only_planar:
-        pca = PCA()
-        keep_molecule = []
-        for positions, charges in zip(qm7_bunch.R, qm7_bunch.Z):
-            transformed = np.vstack([
-                pca.fit_transform(positions[charges != 0]),
-                np.zeros([(charges == 0).sum(), 3])])
-            # the following evaluates how much variance is in the first two axes
-            # before this, the algorithm was also using zero positions, leading
-            # to 454 planar molecules:
-            # pca.fit(positions).explained_variance_ratio_[:2].sum() #  
-            # currently, the algorithm yields 415 planar molecules
-            var_2D = pca.explained_variance_ratio_[:2].sum()
-            keep = (not only_planar) or var_2D > 1 - planarity_tol
-            keep_molecule.append(keep)
-            if align and keep:
-                positions[:] = transformed
+        keep_molecule = _gdb_align(qm7_bunch, align, only_planar, planarity_tol)
            
         if only_planar:
             keep_molecule = np.array(keep_molecule)
@@ -247,6 +280,23 @@ def load_qm7(path=None, align=False, only_planar=False, planarity_tol=.01):
 
     return qm7_bunch
 
+def load_qm9(path=None, align=False, only_planar=False, planarity_tol=.01):
+    filename = _get_or_download_dataset("QM9", path=path, suffix='.tar.gz')
+    qm9_file = _open_pickle(filename)
+    qm9_file['R'] = qm9_file['xyz']
+    qm9_file['T'] = qm9_file['E']
+    qm9_bunch = Bunch(**{k:v for k, v in qm9_file.items()
+        if k in ['R', 'Z', 'T']})
 
+    if align or only_planar:
+        print("processing qm9 molecules, this may take a while...")
+        keep_molecule = _gdb_align(qm9_bunch, align, only_planar, planarity_tol)
+        print("... done.")
 
+        if only_planar:
+            keep_molecule = np.array(keep_molecule)
+            qm9_bunch['T'] = qm9_bunch.T[keep_molecule]
+            qm9_bunch['Z'] = qm9_bunch.Z[keep_molecule]
+            qm9_bunch['R'] = qm9_bunch.R[keep_molecule]
 
+    return qm9_bunch
